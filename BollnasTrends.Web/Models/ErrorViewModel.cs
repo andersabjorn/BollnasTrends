5be7704b27378eefc8c6e24b35b1537c56a65399
Anders Abjorn <anteabjorn@gmail.com>
2026-01-02 21:56:46 +0100

    Initial structure: Core, Infrastructure, Web

---
+++ BollnasTrends.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BollnasTrends.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
